2883d4034376185494b9f266241dbc8d923b8013
radcli14 <radcli14@gmail.com>
2025-09-29 10:15:35 +0200

    created empty query methods

--- txirimiri/txirimiri/ContentManager.swift
+++ txirimiri/txirimiri/ContentManager.swift
@@ -15,4 +15,24 @@ class ContentManager {
         let container = CKContainer(identifier: identifier)
         database = container.publicCloudDatabase
     }
+    
+    // MARK: - Query
+    
+    /// Store models that have been fetched
+    var models = [Model3D]()
+    
+    /// Perform a query for only the `name`, `description`, and `extension` fields for all models available in the database
+    func fetchLightweightRecords() async {
+        
+    }
+    
+    /// Perform a query for `thumbnail` data of a specific record
+    func fetchThumbnail(for name: String) async -> Model3D? {
+        return nil
+    }
+    
+    /// Perform a query for `model` data of a specific record
+    func fetchModel(for name: String) async -> Model3D? {
+        return nil
+    }
 }
