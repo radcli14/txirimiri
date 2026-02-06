95f788f8d9a706dfddf3f51fd986089926ddaec3
radcli14 <radcli14@gmail.com>
2026-02-06 12:55:17 +0100

    fetched namem and description from cloudkit

--- web/txirimiri/static/txirimiri/cloud.js
+++ web/txirimiri/static/txirimiri/cloud.js
@@ -37,3 +37,32 @@ function fetchAllNamesAndDescriptions() {
 }
 
 fetchAllNamesAndDescriptions();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modelList = document.getElementById('model-list');
+    const query = { recordType: 'Model3D' };
+    const options = {
+        desiredKeys: ['name', 'description']
+    };
+    database.performQuery(query, options).then((response) => {
+        if (response.hasErrors) {
+            console.error(response.errors[0]);
+            return;
+        }
+        response.records.forEach(record => {
+            const name = record.fields.name.value;
+            const description = record.fields.description.value;
+            const item = document.createElement('a');
+            item.className = 'list-group-item list-group-item-action';
+            item.innerHTML = generateHTML(name, description);
+            modelList.appendChild(item);
+        });
+    });
+});
+
+function generateHTML(name, description) {
+    return `
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${description}</p>
+    `;
+}   
